6d9560e4e1eaee320710ed826cb5bc05b0ca1dfb
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-02-01 11:45:05 +0500

    create code_1.0

---
+++ HomeWork_1_seminar_6/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.Write(">>> ");
+string? str = Console.ReadLine();
+
+string[] arr = str.Split(",");
+
+int[] arrint = new int[arr.Length];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arrint[i] = Convert.ToInt32(arr[i]);
+}
+
+
+int count = 0;
+
+for (int i = 0; i < arrint.Length; i++)
+{
+    if (arrint[i]>0)
+    {
+        count= count+1;
+    }
+    
+}
+
+System.Console.WriteLine($"\nПользователь ввёл ({count}) числа больше нуля.");
